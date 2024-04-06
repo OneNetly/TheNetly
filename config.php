254cc6bbdb04a0fc83dd31e3feef0f5779aefd6c
OneNetly <admin@onenetly.com>
2024-04-06 16:43:51 +0300

    ok

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $Name       = 'Flimbase';
 $SEOtag     = 'Full HD 1080p 720p 480p Watch or Download for free';
-$URL        = 'https://flimbase.com';
+$URL        = 'http://thenetly.co';
 $description = 'Flimbase is the FREE Movies and TV Series Streaming website, you can watch your favorite content!';
 $keywords   = 'Flimbase,movie download free site';
 $gtag       = 'G-7XWE0XY2FZ'; // example: G-7XWE0XY2FZ
